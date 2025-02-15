303cef8649632b8f866098a8a63c579ef389ef39
github_username_here <email@example.com>
2025-02-14 16:50:10 -0800

    added functionality to find numbers of characters

---
+++ main.py
@@ -0,0 +1,35 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    full_report = get_report(chars_dict)
+    #print(chars_dict)
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_report(chars):
+    
+    for i in chars:
+        if i.isalpha() == True:
+            print(f"The '{i}' character was found {chars[i]} times")
+main()
